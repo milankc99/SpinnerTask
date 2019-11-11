9d3ebb5fc95b961583ae3bd7e6f328cef665751d
Milan KC <milan.kc69@gmail.com>
2019-11-11 15:06:57 +0545

    fourth commit

---
+++ app/src/main/java/com/example/spinnertask/SpinnerDisplay.java
@@ -0,0 +1,36 @@
+package com.example.spinnertask;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpinnerDisplay extends AppCompatActivity{
+    TextView tvName, tvPhone, tvEmail, tvAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spin_display);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        tvName = findViewById(R.id.yName);
+        tvPhone= findViewById(R.id.pNumber);
+        tvEmail = findViewById(R.id.email);
+        tvAddress = findViewById(R.id.address);
+
+        Bundle bundleForm = getIntent().getExtras();
+        if(bundleForm != null){
+            tvName.setText(bundleForm.getString("forName"));
+            tvPhone.setText(bundleForm.getString("forPhone"));
+            tvEmail.setText(bundleForm.getString("forEmail"));
+            tvAddress.setText(bundleForm.getString("forAddress"));
+
+        }
+        else{
+            Toast.makeText(this, "Sorry, no data found.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
